9113d3090271ace8166eacef6888707815c01d72
Roman Frołow <rofrol@gmail.com>
2021-06-11 11:54:52 +0200

    codingmath: draw lines

--- main.js
+++ main.js
@@ -4,5 +4,10 @@ window.onload = function () {
     width = (canvas.width = window.innerWidth),
     height = (canvas.height = window.innerHeight);
 
-  context.fillRect(0, 0, width, height);
+  for (let i = 0; i < 100; i++) {
+    context.beginPath();
+    context.moveTo(Math.random() * width, Math.random() * height);
+    context.lineTo(Math.random() * width, Math.random() * height);
+    context.stroke();
+  }
 };
