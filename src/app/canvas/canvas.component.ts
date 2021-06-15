bd229d0ee10eeb2d6836e516c1780a2bc7fd5e88
Roman Frołow <rofrol@gmail.com>
2021-06-15 17:42:00 +0200

    Works with turtle

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -9,29 +9,222 @@ export class CanvasComponent implements AfterViewInit {
 
   // constructor() { }
 
-  @ViewChild('canvas', { static: true })
+  @ViewChild('canvas', { read: ElementRef })
   canvas!: ElementRef<HTMLCanvasElement>;
+  @ViewChild('canvasTop', { read: ElementRef })
+  canvasTop!: ElementRef<HTMLCanvasElement>;
 
   private ctx!: CanvasRenderingContext2D;
+  private ctxTop!: CanvasRenderingContext2D;
 
   ngAfterViewInit(): void {
     const canvas = this.canvas.nativeElement;
-    if (canvas === null) return;
-    if (canvas.parentElement === null) return;
-    canvas.height = canvas.parentElement.offsetHeight * devicePixelRatio;
-    canvas.width = canvas.parentElement.offsetWidth * devicePixelRatio;
-    this.ctx = this.canvas.nativeElement.getContext('2d')!;
+    canvas.height = canvas.parentElement!.offsetHeight * devicePixelRatio;
+    canvas.width = canvas.parentElement!.offsetWidth * devicePixelRatio;
+    this.ctx = canvas.getContext('2d')!;
+    this.ctx!.scale(devicePixelRatio, devicePixelRatio);
+
+    const canvasTop = this.canvasTop.nativeElement;
+    canvasTop.height = canvasTop.parentElement!.offsetHeight * devicePixelRatio;
+    canvasTop.width = canvasTop.parentElement!.offsetWidth * devicePixelRatio;
+    this.ctxTop = canvasTop.getContext('2d')!;
+    this.ctxTop!.scale(devicePixelRatio, devicePixelRatio);
 
     this.animate();
   }
 
   animate(): void {
-    this.ctx!.scale(devicePixelRatio, devicePixelRatio);
-    // this.ctx!.fillStyle = '#ddd';
-    // const canvas = this.canvas!.nativeElement;
-    // this.ctx!.fillRect(0, 0, canvas.width, canvas.height);
-    this.ctx.fillStyle = 'red';
-    this.ctx.fillRect(100, 100, 125, 125);
-  }
+    const canvas = this.canvas!.nativeElement;
+    const canvasTop = this.canvasTop!.nativeElement;
 
+    // this.ctx!.fillStyle = '#ddd';
+    // this.ctx!.fillRect(0, 0, canvas.width, canvas.height);
+    // this.ctx.fillStyle = 'red';
+    // this.ctx.fillRect(0, 0, 125, 125);
+    // this.ctx.fillStyle = 'blue';
+    // this.ctx.fillRect(0, 0, 125, 125);
+
+
+    const t = turtle({
+      x: canvas.width / 2 / devicePixelRatio,
+      y: canvas.height / 2 / devicePixelRatio,
+      angleInRadians: 0,
+      penDown: false,
+      penColor: "#000000",
+      lineWidth: 1,
+      ctx: this.ctx,
+      canvas,
+      ctxTop: this.ctxTop,
+      canvasTop,
+    });
+
+    t.penDown = true;
+    t.turnright(45);
+    t.forward(60);
+    t.turnright(10);
+    t.forward(30);
+    t.turnright(10);
+    t.forward(30);
+    t.turnright(10);
+    t.forward(30);
+    t.turnright(10);
+    t.forward(30);
+    t.direction(90);
+    t.forward(30);
+    t.direction(180);
+    t.forward(30);
+    t.direction(45);
+    t.forward(30);
+    t.center();
+    t.penwidth(2);
+    t.pencolor("#ff0000");
+    t.direction(90);
+    t.forward(30);
+    t.go(100, 100);
+    t.forward(30);
+    t.gox(30);
+    t.goy(10);
+  };
+}
+
+// TODO: add type annotation for turtle parameter
+function turtle({
+  x,
+  y,
+  angleInRadians,
+  penDown,
+  penColor,
+  lineWidth,
+  ctx,
+  canvas,
+  ctxTop,
+  canvasTop,
+}: any) {
+  // go forward and draw a line along the path if the pen is down
+  const forward = (length: number) => {
+    var x0 = x,
+      y0 = y;
+    x -= length * Math.sin(angleInRadians);
+    y -= length * Math.cos(angleInRadians);
+    if (ctx) {
+      if (penDown) {
+        logStatus();
+        ctx.beginPath();
+        ctx.lineWidth = lineWidth;
+        ctx.strokeStyle = penColor;
+        ctx.moveTo(x0, y0);
+        ctx.lineTo(x, y);
+        ctx.stroke();
+      }
+    } else {
+      ctx.moveTo(x, y);
+    }
+    drawArrowhead(ctxTop, canvasTop, x, y, angleInRadians);
+  };
+  const backward = () => {
+    forward(-length);
+  };
+  const turnleft = (angleInDegrees: number) => {
+    angleInRadians = deg2rad((angleInDegrees + rad2deg(angleInRadians)) % 360);
+    drawArrowhead(ctxTop, canvasTop, x, y, angleInRadians);
+  };
+  const turnright = (angleInDegrees: number) => {
+    turnleft(-angleInDegrees);
+  };
+  const direction = (angleInDegrees: any) => {
+    angleInRadians = 2 * Math.PI - deg2rad(angleInDegrees);
+    drawArrowhead(ctxTop, canvasTop, x, y, angleInRadians);
+  };
+  const center = () => {
+    const penDownPrev = penDown;
+    penDown = false;
+    x = canvas.width / 2 / devicePixelRatio;
+    y = canvas.height / 2 / devicePixelRatio;
+    penDown = penDownPrev;
+    drawArrowhead(ctxTop, canvasTop, x, y, angleInRadians);
+  };
+  const go = (x1: number, y1: number) => {
+    const penDownPrev = penDown;
+    penDown = false;
+    x = canvas.width / 2 / devicePixelRatio + x1;
+    y = canvas.height / 2 / devicePixelRatio + y1;
+    penDown = penDownPrev;
+    drawArrowhead(ctxTop, canvasTop, x, y, angleInRadians);
+  };
+  const gox = (x1: number) => {
+    const penDownPrev = penDown;
+    penDown = false;
+    x = canvas.width / 2 / devicePixelRatio + x1;
+    penDown = penDownPrev;
+    drawArrowhead(ctxTop, canvasTop, x, y, angleInRadians);
+  };
+  const goy = (y1: number) => {
+    const penDownPrev = penDown;
+    penDown = false;
+    y = canvas.height / 2 / devicePixelRatio + y1;
+    penDown = penDownPrev;
+    drawArrowhead(ctxTop, canvasTop, x, y, angleInRadians);
+  };
+  const penwidth = (newLineWidth: number) => {
+    lineWidth = newLineWidth;
+  };
+  const pencolor = (newPencolor: string) => {
+    penColor = newPencolor;
+  };
+  const logStatus = () =>
+    console.log(
+      `"x = ${x}; y = ${y}; angleInRadians = ${angleInRadians}; angleInDegrees = ${rad2deg(
+        angleInRadians
+      )}; penDown = ${penDown}`
+    );
+
+  return {
+    forward,
+    backward,
+    turnleft,
+    turnright,
+    direction,
+    center,
+    go,
+    gox,
+    goy,
+    penwidth,
+    pencolor,
+    set penDown(value: boolean) {
+      penDown = value;
+    },
+    logStatus,
+  };
+}
+
+// https://stackoverflow.com/questions/9705123/how-can-i-get-sin-cos-and-tan-to-use-degrees-instead-of-radians/9705160#9705160
+function deg2rad(deg: number) {
+  return (deg * Math.PI) / 180;
+}
+
+function rad2deg(rad: number) {
+  return (rad * 180) / Math.PI;
+}
+
+// https://stackoverflow.com/questions/16135469/make-pointing-arrow-at-the-end-of-the-drawing-canvas/16137856#16137856
+function drawArrowhead(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement, x: number, y: number, radians: number) {
+  const height = 24;
+  const width = 6;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.save();
+  ctx.beginPath();
+  const x1 = (height / 2) * Math.sin(radians);
+  const y1 = (height / 2) * Math.cos(radians);
+  ctx.translate(x - x1, y - y1);
+  ctx.rotate(-radians);
+  ctx.moveTo(0, 0);
+  ctx.lineTo(width, height);
+  ctx.lineTo(-width, height);
+  ctx.closePath();
+  ctx.restore();
+  ctx.strokeStyle = "#00ff00";
+  ctx.lineWidth = 1;
+  ctx.stroke();
+  // ctx.fillStyle = "#00ff00";
+  // ctx.fill();
 }
