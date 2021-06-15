c2744541e197fee44f818c29644266750246bd0d
Roman Frołow <rofrol@gmail.com>
2021-06-15 17:16:51 +0200

    Refactor

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -10,21 +10,17 @@ export class CanvasComponent implements AfterViewInit {
   // constructor() { }
 
   @ViewChild('canvas', { static: true })
-  canvas: ElementRef<HTMLCanvasElement> | undefined;
+  canvas!: ElementRef<HTMLCanvasElement>;
 
-  private ctx: CanvasRenderingContext2D | undefined;
+  private ctx!: CanvasRenderingContext2D;
 
   ngAfterViewInit(): void {
-    if (this.canvas === undefined) return;
     const canvas = this.canvas.nativeElement;
     if (canvas === null) return;
     if (canvas.parentElement === null) return;
     canvas.height = canvas.parentElement.offsetHeight * devicePixelRatio;
     canvas.width = canvas.parentElement.offsetWidth * devicePixelRatio;
-
-    const ctx = this.canvas.nativeElement.getContext('2d');
-    if (ctx === null) return;
-    this.ctx = ctx;
+    this.ctx = this.canvas.nativeElement.getContext('2d')!;
 
     this.animate();
   }
@@ -34,8 +30,8 @@ export class CanvasComponent implements AfterViewInit {
     // this.ctx!.fillStyle = '#ddd';
     // const canvas = this.canvas!.nativeElement;
     // this.ctx!.fillRect(0, 0, canvas.width, canvas.height);
-    this.ctx!.fillStyle = 'red';
-    this.ctx!.fillRect(100, 100, 125, 125);
+    this.ctx.fillStyle = 'red';
+    this.ctx.fillRect(100, 100, 125, 125);
   }
 
 }
